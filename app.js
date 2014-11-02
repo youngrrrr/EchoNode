6286a1c630b7e8e29bf476dd71a369aa1328cf63
daviddliu <David_Liu@brown.edu>
2014-11-02 03:24:26 -0500

    Updated for aws

--- app.js
+++ app.js
@@ -57,5 +57,9 @@ app.use(function(err, req, res, next) {
     });
 });
 
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
+
 
 module.exports = app;
