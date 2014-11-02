7646d0ac7f427c7b3e0117705e87432f8278f0ce
daviddliu <David_Liu@brown.edu>
2014-11-02 05:26:03 -0500

    UI Changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('echo.db');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { });
+		//Just get the metadata from the top 25 echoes (so text content if it has it, the count, and the distance).
+	var response = {};
+	response.echoboard = [];
+
+	db.all("SELECT * FROM echo WHERE deleted == 0 ORDER BY echoes DESC LIMIT (?)", 25, function(err, rows) {
+		rows.forEach(function(row) {
+			response.echoboard.push(row);
+		});
+  res.render('index', response);
+});
 });
 
 module.exports = router;
